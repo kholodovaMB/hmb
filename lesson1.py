d5c34ca3ccfbff5e6592e41e967a89e3a49cd4d1
kholodovaMB <holodova.maryna@gmail.com>
2021-09-18 17:40:52 +0300

    lesson all

--- lesson1.py
+++ lesson1.py
@@ -1,3 +1,25 @@
-print("hello, world")
-# comment
-print("=",56)
+# print("hello, world")
+# ctrl+alt+L форматирует код под стандарт PEP8
+# ctrl+/    комментировать строки
+# == сравнениие
+# != не равно
+# in проверка на содержание в строке символа
+# not  инверсия
+# float(), int(), str(), bool()  функции приведения типов
+
+value = 5
+test = 2
+# test, value = value, test
+# value_1 = test / value # стандартное деление чисел
+# value_1 = test // value # целочисленное деление (без округления )
+# value_1=test%value # остаток от деления
+# value_1 = test ** value  # возведение в степень
+# value_1=test*value # склейка переменных если символ умножить на число
+# print(value_1)
+# bool_value=2
+# bool_value_1=(2==bool_value)
+# bool_value_1=("qwe" in "qwerty")
+# bool_value_1=("qwe" not in "qwerty")
+bool_value_1=("qwef" in "qwerty")
+bool_value_2=not bool_value_1
+print(bool_value_2)
